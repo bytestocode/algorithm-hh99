c9ab84d1b717f8ee62b1c1e3c1767a9b39d81204
reload1bronze <nature9th@gmail.com>
2022-03-25 00:11:39 +0900

    항해99 알고리즘 2주차 시험 - 코드리뷰 후 불필요한 변수(배열) 삭제

--- 2주차_그래프_트리_비선형_자료구조/test_programmers_43165_타겟넘버_코드리뷰.py
+++ 2주차_그래프_트리_비선형_자료구조/test_programmers_43165_타겟넘버_코드리뷰.py
@@ -1,17 +1,18 @@
 def solution(numbers, target):
     answer = 0
 
-    # 리스트 생성: 합계를 하나씩 계산해서 저장
-    sum_list = list()
-
     # 깊이우선탐색으로 합계를 하나씩 계산
     # 매개변수: 몇번째 숫자인지?, 현재까지 합계는? 2가지 대입
     def dfs(idx, cur_sum):
-        # 인덱스가 주어진 숫자와 같아지면 합계를 구한 것
-        # 합계 리스트에 추가하고 dfs 종료
+        # 인덱스가 주어진 숫자와 같아지면 dfs 종료
+        # target과 같으면 answer +1
         if idx == len(numbers):
-            sum_list.append(cur_sum)
-            return
+            if cur_sum == target:
+                nonlocal answer
+                answer += 1
+                return
+            else:
+                return
 
         # 인덱스에 해당하는 숫자를 더하는 dfs
         sum_plus = cur_sum + numbers[idx]
@@ -25,11 +26,6 @@ def solution(numbers, target):
 
     # 인덱스 0, 합계 0부터 dfs 시작
     dfs(0, 0)
-
-    # 합계 리스트에 4가 있으면 answer +1
-    for i in range(len(sum_list)):
-        if sum_list[i] == target:
-            answer += 1
 
     return answer
 
