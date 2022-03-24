c6e2d01bc9ef46b97556d43e3a9ff7642cec4872
reload1bronze <nature9th@gmail.com>
2022-03-24 11:38:42 +0900

    항해99 알고리즘 2주차 시험 - 코드 정리 -- 주석 작성 -- 변수명 수정 -- 불필요한 줄바꿈 삭제

--- 2주차_그래프_트리_비선형_자료구조/test_programmers_49994_방문길이.py
+++ 2주차_그래프_트리_비선형_자료구조/test_programmers_49994_방문길이.py
@@ -1,54 +1,55 @@
 def solution(dirs):
     answer = 0
+
+    # 현재 좌표 저장
     cur_x = 0
     cur_y = 0
 
-    # 가본 길 저장하는 자료형
-    road = list()
+    # 리스트 생성: 방문한 경로 저장
+    visited_path = list()
 
     for char in dirs:
         if char == "U":
+            # 현재 y 좌표가 5 미만? Up 가능!
             if cur_y < 5:
                 cur_y += 1
-
                 # 처음 가본 길이면 +1
-                if not [[cur_x, cur_y - 1], [cur_x, cur_y]] in road:
-                    road.append([[cur_x, cur_y - 1], [cur_x, cur_y]])
+                if not [[cur_x, cur_y - 1], [cur_x, cur_y]] in visited_path:
+                    visited_path.append([[cur_x, cur_y - 1], [cur_x, cur_y]])
                     answer += 1
-
             else:
                 continue
+
         elif char == "D":
+            # 현재 y 좌표가 -5 초과? Down 가능!
             if -5 < cur_y:
                 cur_y -= 1
-
                 # 처음 가본 길이면 +1
-                if not [[cur_x, cur_y], [cur_x, cur_y + 1]] in road:
-                    road.append([[cur_x, cur_y], [cur_x, cur_y + 1]])
+                if not [[cur_x, cur_y], [cur_x, cur_y + 1]] in visited_path:
+                    visited_path.append([[cur_x, cur_y], [cur_x, cur_y + 1]])
                     answer += 1
-
             else:
                 continue
+
         elif char == "R":
+            # 현재 x 좌표가 5 미만? Right 가능!
             if cur_x < 5:
                 cur_x += 1
-
                 # 처음 가본 길이면 +1
-                if not [[cur_x - 1, cur_y], [cur_x, cur_y]] in road:
-                    road.append([[cur_x - 1, cur_y], [cur_x, cur_y]])
+                if not [[cur_x - 1, cur_y], [cur_x, cur_y]] in visited_path:
+                    visited_path.append([[cur_x - 1, cur_y], [cur_x, cur_y]])
                     answer += 1
-
             else:
                 continue
+
         elif char == "L":
+            # 현재 x 좌표가 -5 초과? Left 가능!
             if -5 < cur_x:
                 cur_x -= 1
-
                 # 처음 가본 길이면 +1
-                if not [[cur_x, cur_y], [cur_x + 1, cur_y]] in road:
-                    road.append([[cur_x, cur_y], [cur_x + 1, cur_y]])
+                if not [[cur_x, cur_y], [cur_x + 1, cur_y]] in visited_path:
+                    visited_path.append([[cur_x, cur_y], [cur_x + 1, cur_y]])
                     answer += 1
-
             else:
                 continue
 
